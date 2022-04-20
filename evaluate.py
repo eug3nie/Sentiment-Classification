556d6c50917d5a656d9c947e5e87a9b755797a83
William Rudman <69358405+wrudman@users.noreply.github.com>
2022-04-20 16:28:08 -0400

    update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -20,27 +20,25 @@ def collate_fn(batch):
     max_len = max([len(f["input_ids"]) for f in batch]) 
     input_ids = [f["input_ids"] + [0] * (max_len - len(f["input_ids"])) for f in batch]
     input_mask = [[1.0] * len(f["input_ids"]) + [0.0] * (max_len - len(f["input_ids"])) for f in batch]
-    labels = [f["labels"] for f in batch]
     input_ids = torch.tensor(input_ids, dtype=torch.long)
     input_mask = torch.tensor(input_mask, dtype=torch.float)
-    labels = torch.tensor(labels, dtype=torch.long)
-    outputs = { "input_ids": input_ids, "attention_mask": input_mask, "labels": labels, }
+    outputs = { "input_ids": input_ids, "attention_mask": input_mask}
     return outputs
-    
-    
-def eval(args, data, model, tokenizer, test=False):
+      
+def eval(args, data, model, tokenizer):
     # Set model to eval mode. Load metric and create data loader.  
+    # If batch_size = 1 can get rid of collate_rn. 
     print("Evaluating") 
-    model.eval() 
     eval_loader = DataLoader(data, batch_size=args.batch_size, collate_fn=collate_fn)
     
     # Send model to gpu if available
     device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
     model.to(device)
+    model.eval()
     
     # Lists to store results. 
     preds_list = []
-    labels_list = []
+    probs_list = []
     
     # main EVAL loop 
     for idx, batch in enumerate(eval_loader):
@@ -48,29 +46,21 @@ def eval(args, data, model, tokenizer, test=False):
         batch = {key: value.to(device) for key, value in batch.items()} 
        
         # Set model to eval and run input batches with no_grad to disable gradient calculations   
-        model.eval()
         with torch.no_grad():
             outputs = model(**batch) 
             logits = outputs.logits   
-       # Store Predictions and Labels
+       
+        # Store Predictions
         preds = logits.argmax(axis=1)        
         preds = preds.detach().cpu().numpy()  
         preds_list.append(preds)   
-        labels = batch["labels"].detach().cpu().numpy() 
-        labels_list.append(labels)  
+        
+        #Probabilities
         probs = torch.nn.functional.softmax(logits, dim=1)  
-         
-        # Get indices of correct and incorrect labels 
-        correct_indx = np.where(preds == labels)[0] 
-        incorrect_indx = list(np.where(preds != labels)[0]) 
-        ids = batch['input_ids'] 
-    
-    # Compute Accuracy 
-    preds = np.concatenate(preds_list, axis=0)
-    labels = np.concatenate(labels_list, axis=0)
-    acc = (preds == labels).sum()/len(preds)
-
-    return acc
+        probs = probs.detach().cpu().numpy()  
+        probs_list.append(probs)
+        
+    return probs_list, preds_list
     
     def main():
     parser = argparse.ArgumentParser()
@@ -81,8 +71,7 @@ def eval(args, data, model, tokenizer, test=False):
     
     # Load data 
     # TODO: CHANGE TO OUR DATA
-    data = load_dataset("glue", args.task)
-    num_labels = len(data["train"].features["label"].names) 
+    data = #TODO import data
     print("Data loaded") 
     
     #Sow seeds 
@@ -105,24 +94,16 @@ def eval(args, data, model, tokenizer, test=False):
     model.config.pad_token_id = model.config.eos_token_id
 
     print("Preprocessing Data")
-    # preprocess data   
-   
-    #TODO: THIS FUNCTION WILL GET THE DATA IN APPROPRIATE FORMAT AS A DICTIONARY WITH LABELS AND TOKENIZED TEXT
+    #TODO: THIS FUNCTION WILL GET THE DATA IN APPROPRIATE FORMAT AS A DICTIONARY TOKENIZED TEXT
     #TODO: THIS NEEDS TO BE DONE FROM SCRATCH
     def preprocess(example): 
-        key1, key2 = task_keys[args.task] 
-        if key2 is None: inputs = (example[key1],)
-        else: 
-            inputs = (example[key1], example[key2])
         tokenizer.pad_token = tokenizer.eos_token 
-        results = tokenizer(*inputs, max_length=256, truncation=True, padding=True) 
-        results["labels"] = example["label"] #if "label" in example else 0  
+        results = tokenizer(*inputs, max_length=100, truncation=True, padding=True) 
         return results
  
-    eval_data = list(map(preprocess, data["validation"])) 
-   
-    results = eval(args=args, data=eval_data, model=model, tokenizer=tokenizer, test=True) 
-    print("TESTING ACCURACY: ", results)
+    #CHANGE TO OUR DATASET
+    eval_data = list(map(preprocess, data)) 
+    results = eval(args=args, data=eval_data, model=model, tokenizer=tokenizer) 
     
 if __name__  == "__main__":
     main()
